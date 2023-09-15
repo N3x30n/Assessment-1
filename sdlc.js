0456aeb99524e482011de8a1b8d41d33593b7be7
Atlas Tran <atlas.tran123@gmail.com>
2023-09-15 10:01:23 -0600

    sdlc finished

--- sdlc.js
+++ sdlc.js
@@ -19,3 +19,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const  Analysis = "This is the second step, in which the team gathers information and client requirments for the product. This stage is meant for the team to fully understand the minute details of the project and its requirments."
+
+const design = "This is the third step, in which the program developer will make sure all the details of the project and requirments are met before choosing how the product will look and what software to use for said project."
+
+const Implementation = "This is the fourth step, in which the team will start coding and implementing the project for the clients use."
+
+const Testing = "This is the fifth step, in which the client and programmers will test if the product meets the requirements and can be implemented with the rest of the project or preexisting product."
+
+const Maintain = "This is last step, in which the team will maintain the project to make sure all the bugs and kinks are removed from the product to allow the user smooth access to the product."
